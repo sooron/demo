5cd2855487d5f9f8011eeb0f3d257b6ef94502a4
Sooron <11066709+sooron@users.noreply.github.com>
2020-08-15 12:00:31 +0200

    Added custom property from logback.xml to pattern class example

--- src/main/java/com/example/demo/layouts/PatternLayoutCustom.java
+++ src/main/java/com/example/demo/layouts/PatternLayoutCustom.java
@@ -4,6 +4,10 @@ import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
 public class PatternLayoutCustom  extends PatternLayout {
+
+    /* this property will be set via logback.xml configuration file */
+    private String flavor;
+
     @Override
     public String doLayout(ILoggingEvent event) {
         return doTransform(super.doLayout(event));
@@ -12,7 +16,17 @@ public class PatternLayoutCustom  extends PatternLayout {
     /* Message transformation */
     private String doTransform(String msg) {
         /* Do whatever transformation is needed here, remember log message ends with \n */
-        String resultMsg = msg.replaceAll("DEBUG", "LEMON").replaceAll("INFO", "APPLE").replaceAll("WARN", "ORANGE").replaceAll("ERROR", "STRAWBERRY");
+        String resultMsg = msg;
+        switch (flavor) {
+            case "fruits" :
+                resultMsg = msg.replaceAll("DEBUG", "LEMON").replaceAll("INFO", "APPLE").replaceAll("WARN", "ORANGE").replaceAll("ERROR", "STRAWBERRY");
+                break;
+        }
         return resultMsg;
+    }
+
+    /* custom properties need setters to be set from configuration file logback.xml */
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
     }
 }
